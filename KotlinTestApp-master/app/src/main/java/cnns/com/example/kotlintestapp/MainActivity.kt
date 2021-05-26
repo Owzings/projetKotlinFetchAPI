26d25d0502c2e8c30aec15af67b11ff5b6c542ac
Ali Es-skouri <38128431+Owzings@users.noreply.github.com>
2021-05-26 22:05:04 +0200

    Add files via upload

--- KotlinTestApp-master/app/src/main/java/cnns/com/example/kotlintestapp/MainActivity.kt
+++ KotlinTestApp-master/app/src/main/java/cnns/com/example/kotlintestapp/MainActivity.kt
@@ -1,39 +1,63 @@
 package cnns.com.example.kotlintestapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity(), ExampleAdapter.OnItemClickListener {
-    private val exampleList = generateDummyList(500)
+    private val exampleList = generateDummyList(500, null)
     private val adapter = ExampleAdapter(exampleList, this)
+
+    private var PRIVATE_MODE = 0
+    private val PREF_NAME = "Set"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Système de cache
+        val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
-        startProperly();
+        startProperly(sharedPref);
     }
-    fun startProperly(){
+
+    public override fun onResume() {
+        val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        super.onResume()
+        generateDummyList(500, sharedPref)
+        val newItem = ExampleItem(
+            R.drawable.ic_android,
+            "Caching",
+            "-"
+        )
+        exampleList.add(0, newItem)
+        adapter.notifyItemInserted(0)
+    }
+
+
+    fun startProperly(sharedPref: SharedPreferences?){
         val newItem = ExampleItem(
             R.drawable.ic_android,
             "Départ",
             "Ceci est un élément template, cliquez sur fetcher !"
         )
         exampleList.add(0, newItem)
+        generateDummyList(500, sharedPref)
         adapter.notifyItemInserted(0)
     }
     fun insertItem(view: View) {
@@ -72,9 +96,10 @@ class MainActivity : AppCompatActivity(), ExampleAdapter.OnItemClickListener {
         clickedItem.text1 = "Clicked"
         startActivity(intent)
     }
-     fun generateDummyList(size: Int): ArrayList<ExampleItem> {
+     fun generateDummyList(size: Int,  sharedPref: SharedPreferences?): ArrayList<ExampleItem> {
 
         val list = ArrayList<ExampleItem>()
+
         for (i in 0 until size) {
             val drawable = when (i % 3) {
                 0 -> R.drawable.ic_android
@@ -87,7 +112,7 @@ class MainActivity : AppCompatActivity(), ExampleAdapter.OnItemClickListener {
 
                     if (response.isSuccessful && response.body() != null) {
                         val content = response.body()
-                        println(content)
+//                        println(content)
                         val item = ExampleItem(drawable, "Item $i", content?.title)
                         list += item
 
@@ -109,6 +134,22 @@ class MainActivity : AppCompatActivity(), ExampleAdapter.OnItemClickListener {
                 }
             }
         }
+         if(sharedPref != null) {
+             val restoredText = sharedPref.getString("Set", null);
+             if (restoredText != null) {
+                 val gson = Gson()
+                 val json = gson.toJson(list)
+                 var test: Array<ExampleItem> =
+                     gson.fromJson(json, Array<ExampleItem>::class.java).reversedArray()
+                         .reversedArray();
+                 return test.toCollection(ArrayList());
+             } else {
+                 val gson = Gson()
+                 val json = gson.toJson(list)
+                 sharedPref.edit().putString("Set", json)
+             }
+         }
+
 
         return list
     }
